29ce3dd18c53c0da1fb048667dffc67a8e127a29
Cinkowyy <90154960+Cinkowyy@users.noreply.github.com>
2022-02-01 15:11:52 +0100

    Adds error message text in main view

--- src/main/java/todoapp/controllers/MainController.java
+++ src/main/java/todoapp/controllers/MainController.java
@@ -1,14 +1,23 @@
 package todoapp.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import todoapp.modules.AuthKey;
+import todoapp.modules.LoginErrorMessage;
 import todoapp.modules.Todo;
 
 public class MainController {
 
     @FXML
+    private TextField todoInput;
+
+    @FXML
     private VBox todosContainer;
+
+    @FXML
+    private Text errorMessage;
 
     public MainController(AuthKey authKey) {
         System.out.println("MainController started with key:");
@@ -17,8 +26,12 @@ public class MainController {
 
     @FXML
     void initialize() {
+
+        LoginErrorMessage loginMessageController = new LoginErrorMessage(errorMessage);
+
         Todo todo = new Todo(2, "Make this shit work", 1);
         todosContainer.getChildren().add(todo.renderTask());
+
     }
 
 }
