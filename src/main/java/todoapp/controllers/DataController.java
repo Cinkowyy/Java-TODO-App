06189b26fb20effc794db08e5fda4e2373c86a88
Cinkowyy <90154960+Cinkowyy@users.noreply.github.com>
2022-02-07 22:17:27 +0100

    Adds todo in DB status update

--- src/main/java/todoapp/controllers/DataController.java
+++ src/main/java/todoapp/controllers/DataController.java
@@ -1,9 +1,33 @@
 package todoapp.controllers;
 
+import com.google.gson.Gson;
+import todoapp.modules.Message;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public abstract class DataController {
 
+    static Gson gson = new Gson();
+
     public static void updateStatus(int todoId, boolean newStatus) {
-        System.out.println("Task id: "+ todoId+" new status is: "+ newStatus);
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://localhost:3000/update"))
+                .header("Content-Type", "application/json")
+                .method("POST", HttpRequest.BodyPublishers.ofString("{\n\t\"id\":" + todoId +",\n\t\"status\": "+ newStatus+"\n}"))
+                .build();
+        HttpResponse<String> response = null;
+        try {
+            response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+            Message resMessage = gson.fromJson(response.body(), Message.class);
+            System.out.println(resMessage.message);
+
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void deleteTodo(int todoId) {
